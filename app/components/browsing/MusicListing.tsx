ae6e1011c92e8300be00a9389f7993ebaaecbd1e
Coliniloc01 <nordmarkcolin@gmail.com>
2023-11-09 11:09:45 +0100

    fix: Changed tag of musicListing to button

--- app/components/browsing/MusicListing.tsx
+++ app/components/browsing/MusicListing.tsx
@@ -13,9 +13,9 @@ export function MusicListing({
 }:Props) {
   
   return (
-    <article className='flex flex-col border-2 border-white p-3 mt-4 rounded-lg w-[100%] justify-between'>
+    <button onClick={() => console.log('hello')} className='flex flex-col border-2 border-white p-3 mt-4 rounded-lg w-[100%] justify-between'>
           <h2>{name}</h2>
           <h2>{artist}</h2>
-    </article>
+    </button>
   );
 }
